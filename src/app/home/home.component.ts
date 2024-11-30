2cb33c95556459a4f938e6eceb400e4eac0b6fe6
Christian <oficialsteffens@hotmail.com>
2024-11-30 19:26:36 -0300

    install prime angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormsModule, ButtonModule, CalendarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  date: Date | undefined;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const updatedAppConfig = {
+  ...appConfig,
+  providers: [...(appConfig.providers || []), provideAnimations()],
+};
+
+bootstrapApplication(AppComponent, updatedAppConfig).catch((err) =>
+  console.error(err)
+);
